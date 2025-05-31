d8300cb3570f11fdbec8f25223966ea6ee7a535c
zzkhong <khongc7@gmail.com>
2025-05-31 13:46:06 +0800

    feat: add rn revision note

--- src/components/window/ContentResource.tsx
+++ src/components/window/ContentResource.tsx
@@ -2,43 +2,13 @@
 
 import React from "react";
 import DesktopFile from "../file/DesktopFile";
+import files from "@/constants/fileData";
 
 interface ContentResourceProp {
   onRedirect: (msg: string, url: string) => void;
 }
 
 const ContentResource: React.FC<ContentResourceProp> = ({ onRedirect }) => {
-  const files = [
-    {
-      id: "leetcode",
-      name: "Leetcode Blind 75",
-      type: "internet",
-      link: "https://zzkhong.notion.site/LeetCode-77-6dbfacb05b294cb19843e9c71c816811?pvs=74",
-      message: "Opening LeetCode Blind 75",
-    },
-    {
-      id: "systemdesign",
-      name: "Awesome System Design",
-      type: "internet",
-      link: "https://github.com/ashishps1/awesome-system-design-resources",
-      message: "Opening Awesome System Design",
-    },
-    {
-      id: "books",
-      name: "Books Catalogue",
-      type: "internet",
-      link: "https://github.com/rishabhmodi03/BOOKS",
-      message: "Opening Books Catalogue",
-    },
-    {
-      id: "design_pattern",
-      name: "Design Pattern",
-      type: "internet",
-      link: "https://refactoring.guru/design-patterns/go",
-      message: "Opening Design Pattern",
-    },
-  ];
-
   return (
     <div className="flex flex-row py-4 text-green-400">
       {files.map((file) => (
---
+++ src/constants/fileData.ts
@@ -0,0 +1,39 @@
+const files = [
+  {
+    id: "leetcode",
+    name: "Leetcode Blind 75",
+    type: "internet",
+    link: "https://zzkhong.notion.site/LeetCode-77-6dbfacb05b294cb19843e9c71c816811?pvs=74",
+    message: "Opening LeetCode Blind 75",
+  },
+  {
+    id: "systemdesign",
+    name: "Awesome System Design",
+    type: "internet",
+    link: "https://github.com/ashishps1/awesome-system-design-resources",
+    message: "Opening Awesome System Design",
+  },
+  {
+    id: "books",
+    name: "Books Catalogue",
+    type: "internet",
+    link: "https://github.com/rishabhmodi03/BOOKS",
+    message: "Opening Books Catalogue",
+  },
+  {
+    id: "design_pattern",
+    name: "Design Pattern",
+    type: "internet",
+    link: "https://refactoring.guru/design-patterns/go",
+    message: "Opening Design Pattern",
+  },
+  {
+    id: "rn_interview",
+    name: "React Native Revision",
+    type: "internet",
+    link: "https://www.notion.so/zzkhong/Notes-20305f97069b80b69822ffb388e351dc?source=copy_link",
+    message: "Opening RN Revision",
+  },
+];
+
+export default files;
